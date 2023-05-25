ef59b02f862909c0b34b91c54e570f754f829b65
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2023-05-25 08:55:21 +0300

    added templates

--- store/store/settings.py
+++ store/store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$(cj59-i&)#kx56pu_^j29a$l!dronz6kc!i0ra4uw$stzxp#%'
+SECRET_KEY = 'django-insecure-70_r+mtf8zasvcl#42duf^#$i6cp@p_klmt_+3i%q78n4izlf@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR/'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
