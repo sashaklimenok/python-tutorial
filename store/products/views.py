251db47c7597327aae3b9eaebf78b07a770b08c0
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2023-05-25 16:35:44 +0300

    updatetd templates

--- store/products/views.py
+++ store/products/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'Store title'}
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/product.html')
+    context = {
+        'title': 'Store - Products',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            }
+        ]
+    }
+    return render(request, 'products/product.html', context)
