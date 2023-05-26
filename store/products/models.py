abb5738ee971acdb9e9805a7348250e0fbe03530
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2023-05-26 09:46:26 +0300

    deleted migrations

--- store/products/models.py
+++ store/products/models.py
@@ -5,11 +5,14 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return f'Category: {self.id} | Name: {self.name}'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 
 def index(request):
@@ -9,13 +10,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - Products',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090,
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
-            }
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
     }
     return render(request, 'products/product.html', context)
